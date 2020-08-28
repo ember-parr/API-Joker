15e94a1f0b7b78e121c30483238d0eb1b964bc4b
Ember Parr <ember-devops@gmail.com>
2020-08-28 11:06:06 -0500

    joke shows in console api working

--- scripts/Joke.js
+++ scripts/Joke.js
@@ -1,2 +1,14 @@
 // Define and export a function called Joke.
 // It should accept a joke object as a parameter and return an html string
+
+export const JokeSetup = (jokeObj) => {
+  return `
+    ${jokeObj.setup}
+    `;
+};
+
+export const JokePunchline = (jokeObj) => {
+  return `
+    ${jokeObj.punchline}
+    `;
+};
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,15 @@
+import { useJoke } from "./JokeProvider.js";
+import { JokeSetup, JokePunchline } from "./Joke.js";
+
 const jokeBtn = document.querySelector(".request-joke-btn");
+
+const setupElement = document.querySelector(".joke-setup");
+const punchlineElement = document.querySelector(".joke-punchline");
 
 jokeBtn.addEventListener("click", (e) => {
   console.log("I should be grabbing a joke now...");
+  useJoke();
+  JokeSetup();
+  JokePunchline();
   // TODO: fetch a joke from the Joke API and render it to the DOM
 });
